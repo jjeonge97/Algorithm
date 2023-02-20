7e36378a3dbd104199824055e369ee7c36f702fe
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-20 22:27:53 +0900

    [level 0] Title: 모음 제거, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,8 @@
 function solution(my_string) {
+    // var answer = ['a', 'e','i','o','u']
+    const answer = 'aeiou'
+    return my_string.split('').filter(el => answer.indexOf(el) === -1).join('')
 
-return my_string.split('').filter(el => el !== 'a' && el !== 'e' && el !== 'i' && el !== 'o' && el !=='u').join('')
+    // return my_string.replace(/[aeiou]/g , '')
 
 }
