77d0cc099c5148305a756220f96c0ed27f9a3968
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-20 09:34:38 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -2,7 +2,7 @@ function solution(my_string) {
     var answer = 0;
     for(let i=0; i < my_string.length; i++) {
         if(!isNaN(my_string[i])) {
-            answer = answer + Number(my_string[i])
+            answer = answer + +(my_string[i])
         }
     }
     return answer;
