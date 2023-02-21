8ee99d76138654b1c2588ba3fbeed4137a61c267
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-21 14:29:16 +0900

    [level 0] Title: 가까운 수, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,14 +1,10 @@
 function solution(array, n) {
-    let first = 0;
-    let last = 0;
-    let newArr = array.map((el) => Math.abs(n - el));
-    let min = Math.min(...newArr);
-    first = newArr.indexOf(min);
-    last = newArr.lastIndexOf(min);
+    let answer = 0;
+    let newArr = array.sort((a,b) => a -b).map((el) => Math.abs(n - el))
 
-  if (array[first] < array[last]) {
-    return array[first];
-  } else {
-    return array[last];
-  }
+    let min = Math.min(...newArr);
+    answer = newArr.indexOf(min);
+
+    return array[answer];
+  
 }
