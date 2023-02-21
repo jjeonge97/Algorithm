73473aae9ae92d6d38b1d6e6ebf4130b74d1d501
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-21 21:11:43 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,4 +1,4 @@
 function solution(order) {
-    var answer = String(order).split('').filter(el => el === "3" || el === "6" || el === "9").length
-    return answer;
+    let game = '369'
+    return String(order).split('').filter(el => game.includes(el)).length
 }
