fd588773d2c322cc941462b33b69df643b97960b
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-24 21:23:07 +0900

    [level 0] Title: 암호 해독, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,7 @@
 function solution(cipher, code) {
     var answer = '';
-   for(let i = 0; i < cipher.length; i ++) {
-       if( (i + 1) % code === 0) answer += cipher[i]
-   }
+    for(let i = code - 1; i < cipher.length; i += code) {
+        answer += cipher[i]
+    }
     return answer;
 }
-// arr의 Index 는 0부터 시작
-// 문제에선 index 1부터 시작
