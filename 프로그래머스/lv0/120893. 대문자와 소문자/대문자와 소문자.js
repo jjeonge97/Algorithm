aa6354d1623b8360ccd60258b6ae504b94998af0
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-24 21:25:42 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-return my_string.split('').map(el => (el === el.toUpperCase()) ? el.toLowerCase() : el.toUpperCase()).join('')
+return my_string.split('').map(el => el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()).join('')
 }
