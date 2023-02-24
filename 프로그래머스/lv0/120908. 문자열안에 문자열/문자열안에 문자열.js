364ab54bff6b89704a9c6045ae9c865363b44253
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-02-24 09:05:18 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,6 +1,8 @@
 function solution(str1, str2) {
 
-    if(str1.includes(str2)) return 1
+//     if(str1.includes(str2)) return 1
     
-    return 2;
+//     return 2;
+    
+    return str1.includes(str2) ? 1 : 2
 }
