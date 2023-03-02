83258dac9908155520c4f6974306e7535d4e9b63
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-02 19:56:39 +0900

    [level 1] Title: 모의고사, Time: 1.71 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,11 +1,14 @@
 function solution(answers) {
     let answer = []
-    let one = [1,2,3,4,5] // 5
-    let two = [2,1,2,3,2,4,2,5] // 8
-    let three = [3,3,1,1,2,2,4,4,5,5] // 10
-    let oneCnt = answers.filter((el,index) => answers[index] === (one[index % one.length])).length
-    let twoCnt = answers.filter((el,index) => answers[index] === (two[index % two.length])).length
-    let threeCnt = answers.filter((el,index) => answers[index] === (three[index % three.length])).length
+    let one = [1,2,3,4,5] 
+    let two = [2,1,2,3,2,4,2,5] 
+    let three = [3,3,1,1,2,2,4,4,5,5] 
+    // let oneCnt = answers.filter((el,index) => el === one[index]).length
+    // let twoCnt = answers.filter((el,index) => el === two[index]).length
+    // let threeCnt = answers.filter((el,index) => el === three[index]).length
+    let oneCnt = answers.filter((el,index) => el === (one[index % one.length])).length
+    let twoCnt = answers.filter((el,index) => el === (two[index % two.length])).length
+    let threeCnt = answers.filter((el,index) => el === (three[index % three.length])).length
 
     let max = Math.max(oneCnt, twoCnt, threeCnt)
     let result = [oneCnt,twoCnt,threeCnt]
@@ -15,7 +18,6 @@ function solution(answers) {
             answer.push(i +1)
         }
     }
-
 return answer
 }
 
