e06cbc10e3c137393f3eb4cf548e3d9597110851
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-03 20:29:54 +0900

    [level 1] Title: K번째수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,5 +1,6 @@
 function solution(array, commands) {
-    let answer = [];
-    // return arr.slice(commands[0] - 1,commands[1]).sort((a,b) => a - b)[commands[2] - 1]
-    return commands.map(el => array.slice(el[0] -1, el[1]).sort((a,b) => a - b)[el[2] - 1])
+    // var answer = [];
+    // return answer;
+    
+   return commands.map(el => array.slice(el[0] - 1, el[1]).sort((a,b)=> a -b)[el[2] - 1])
 }
