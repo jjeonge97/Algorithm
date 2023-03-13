135495d2db4e5ad2f4d3ebc346fcd48ed4621b6d
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-13 09:08:32 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+
+    const str = [...my_string]
+    str.splice(num1,1,my_string[num2])
+    str.splice(num2,1,my_string[num1])
+    return str.join('')
+}
+
+
