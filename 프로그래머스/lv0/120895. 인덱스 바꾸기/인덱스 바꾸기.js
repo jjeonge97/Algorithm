89e9b9ad2aebef58abd4e3b8376f51af774c13e8
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-13 09:14:19 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,9 +1,13 @@
 function solution(my_string, num1, num2) {
 
     const str = [...my_string]
+    let temp = str[num1]
+    str[num1] = str[num2]
+    str[num2] = str[num1]
     str.splice(num1,1,my_string[num2])
     str.splice(num2,1,my_string[num1])
+    //return str.join('')
     return str.join('')
 }
 
-
+// temp에 저장해서 하는건 왜 안돼?
