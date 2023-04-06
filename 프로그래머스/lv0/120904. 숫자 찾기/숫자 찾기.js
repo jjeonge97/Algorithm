1f46f3b9d2be275e332d56e8fd6a6f74618819c8
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-06 09:32:11 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,5 +1,6 @@
 function solution(num, k) {
-    let str = String(num).split('')
+    // let str = String(num).split('')
+    // return str.includes(String(k)) ? str.indexOf(String(k)) + 1 : -1
+    let str = String(num)
     return str.includes(String(k)) ? str.indexOf(String(k)) + 1 : -1
-
 }
