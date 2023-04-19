00ee7366eedf13353ccc710d9d676be4de01cc9d
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-19 09:49:51 +0900

    [level 2] Title: 튜플, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -2,12 +2,12 @@ function solution(s) {
     var answer = [];
     s = s.replace("{{", "").replace("}}", "").split("},{").sort((a,b) => a.length - b.length)
     
-    let arr = [] // [['2'],['2','1'],['1','2','3'],['1','2','3','4']]
+    let arr = [] 
     for(let i = 0; i < s.length; i++) {
         arr.push(s[i].split(','))
     }
     
-    answer.push(parseInt(arr[0][0])) // answer = ['2']
+    answer.push(parseInt(arr[0][0])) 
     
     for(let i = 1; i < arr.length; i++) {
         for(let j = 0; j < arr[i].length; j++) {
@@ -19,3 +19,14 @@ function solution(s) {
     
     return answer;
 }
+
+// let newArr = JSON.parse(s.replace(/{/g,'[').replace(/}/g,']'));
+//     newArr.sort((a, b) => {return a.length - b.length});
+//     let temp = [];
+//     for(let i = 0; i < newArr.length; i++) {
+//         for(let j = 0; j < newArr[i].length; j++) {
+//             temp.push(newArr[i][j]);
+//         }
+//     }
+//     answer = [...new Set(temp)];
+//     return answer;
