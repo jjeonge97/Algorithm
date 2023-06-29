f2d2cefed06607188918c2c88e5add722eab2b33
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-06-29 10:19:56 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,15 @@
+function solution(babbling) {
+    let words = ['aya', 'ye', 'woo', 'ma']
+    let count = 0;
+    
+    for(let i = 0; i < babbling.length; i++) {
+        for(let j = 0; j < words.length; j++) {
+            if(babbling[i].includes(words[j])) {
+               babbling[i] = babbling[i].replace(words[j],'x')
+            }
+        }
+    }
+
+   return babbling.map(el => el.replaceAll('x', '')).filter(el => el === '').length
+
+}
