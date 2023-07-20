7147d454f6fcd2da9be15e357395e5b15a86ce12
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-07-20 10:04:05 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,18 +1,23 @@
 function solution(dot) {
-    let a = {
-        1 : 0,
-        2 : 0,
-        3 : 0,
-        4 : 0
-    }
+//     let a = {
+//         1 : 0,
+//         2 : 0,
+//         3 : 0,
+//         4 : 0
+//     }
     
-    if(dot[0] > 0) a['1'] += 1, a['4'] += 1
-    else a['2'] += 1, a['3'] += 1
+//     if(dot[0] > 0) a['1'] += 1, a['4'] += 1
+//     else a['2'] += 1, a['3'] += 1
     
-    if(dot[1] > 0) a['1'] += 1, a['2'] += 1
-    else a['3'] += 1, a['4'] += 1
+//     if(dot[1] > 0) a['1'] += 1, a['2'] += 1
+//     else a['3'] += 1, a['4'] += 1
     
-    for(let el in a) {
-        if(a[el] === 2) return Number(el)
-    }
+//     for(let el in a) {
+//         if(a[el] === 2) return Number(el)
+//     }
+    
+    const [num1, num2] = dot;
+    const check = num1 * num2 > 0;
+    
+    return num1 > 0 ? (check ? 1 : 4) : (check ? 3: 2)
 }
