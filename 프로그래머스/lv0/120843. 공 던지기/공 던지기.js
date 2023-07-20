11f0e80bbd0bd5391b608ee50ccae4e47af37a5f
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-07-20 10:51:29 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -1,7 +1,7 @@
 function solution(numbers, k) {
     let answer = 1;
 
-    for(let i = 0; i < k -1; i++) { 
+    for(let i = 1; i < k; i++) { 
         answer += 2 
         
         if(answer > numbers.length) {
