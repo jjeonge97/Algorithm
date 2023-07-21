405886bf95ab0fce65a1db17aca5906fda942702
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-07-21 11:03:28 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,17 +1,33 @@
 function solution(n) {
-    var answer = 0;
-    for(let i = 4; i <= n; i ++) {
-        let count = 0
-        for(let j = 1; j <= i; j++) {
-            if(i % j === 0) {
-                count++
-            }
+//     var answer = 0;
+//     for(let i = 4; i <= n; i ++) {
+//         let count = 0
+//         for(let j = 1; j <= i; j++) {
+//             if(i % j === 0) {
+//                 count++
+//             }
+//         }
+        
+//         if(count >= 3) {
+//             answer ++
+//             count = 0
+//         }
+//     }
+//     return answer;
+    let answer = 0;
+    function isPrime(num) {
+        if(num <= 2) return true;
+        
+        for(let i = 2; i < num; i++) {
+            if(num % i === 0) return false;
         }
         
-        if(count >= 3) {
-            answer ++
-            count = 0
-        }
+        return true;
     }
-    return answer;
+    
+    for(let i = 1; i <= n; i++) {
+        if(isPrime(i)) answer++;
+    }
+    
+    return n - answer
 }
