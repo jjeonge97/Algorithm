76c4b5ec5ca65bcd935586bb1db135d07cf48091
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-07-24 10:28:08 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,5 +1,4 @@
 function solution(my_string) {
-    
-    //return my_string.split('').map(el => el === 0 ? 0 : parseInt(el))
+
     return my_string.split('').filter(el => !isNaN(el)).map(el => +el).sort()
 }
